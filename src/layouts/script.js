09914d1e15a6a10212b59abaf0617db3c32a1e87
TanujPandey09 <90381787+TanujPandey09@users.noreply.github.com>
2024-02-22 13:12:05 +0530

    Slug Update

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -6,7 +6,7 @@
   document.addEventListener("mousemove", function (e) {
     var x = e.clientX;
     var y = e.clientY;
-    cursor.style.transfor m = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
   });
 
   document.addEventListener("mousemove", function (e) {
